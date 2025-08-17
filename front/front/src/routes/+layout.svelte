b2acb4892440f6930ec97db96e87dc80b6753884
khan <khan>
2025-08-17 19:39:38 +0100

    wip

---
+++ front/front/src/routes/+layout.svelte
@@ -0,0 +1,14 @@
+<script lang="ts">
+	import '../app.css';
+	import favicon from '$lib/assets/favicon.svg';
+	
+	let { children } = $props();
+</script>
+
+<svelte:head>
+	<link rel="icon" href={favicon} />
+</svelte:head>
+
+<main class="text-white min-h-screen min-w-screen flex justify-center items-center">
+	{@render children?.()}
+</main>
---
+++ front/front/src/routes/+page.svelte
@@ -0,0 +1,43 @@
+<script lang="ts">
+</script>
+
+<div class=" min-h-screen w-full grid grid-rows-[auto,1fr,auto]">
+	<div class="">top</div>
+	<div class="relative flex justify-center justify-items-center w-4/5 -m-1">
+		middle
+		<br/>
+		<input
+			type="text"
+			name=""
+			id=""
+			placeholder="foo"
+			class="
+				absolute
+				w-full
+				shadow-sm
+				shadow-blue-100
+				rounded-full
+				text-4xl
+				text-gray-100
+				px-5
+				z-10
+			"
+		/>
+		<div
+			class="
+				absolute
+				w-full
+				shadow-md
+				shadow-emerald-400
+				rounded-full
+				text-4xl
+				text-gray-100
+				px-5
+				z-0
+			"
+		><p class="opacity-0">
+			.
+		</p></div>
+	</div>
+	<div class="">bottom</div>
+</div>
