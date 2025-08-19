bea764e72d12e052933385c6888721df308198cc
khan <khan>
2025-08-19 18:00:25 +0100

    wip

--- front/front/src/routes/+page.svelte
+++ front/front/src/routes/+page.svelte
@@ -1,35 +1,23 @@
 <script lang="ts">
-	import type { Attachment } from 'svelte/attachments';
+	import type { Attachment } from "svelte/attachments";
 	let x = $state(0);
 	let y = $state(0);
+	let circleClip: HTMLDivElement | null = null;
 
 	function captureMouse(e: MouseEvent) {
 		// console.log("x: ", e.x, "y: ", e.y);
-		x = e.x
-		y = e.y
+		if (circleClip != null){
+			let _x: string , _y: string = [circleClip.style.x, circleClip.style.y]
+			x = e.x - parseInt(_x)};
+			y = e.y - parseInt(circleClip.style.y)};
+		}
 		console.log("x: ", x, "y: ", y);
-
 	}
-
 </script>
 
 <svelte:window onmousemove={captureMouse} />
 
-<div
-	class="
-	fixed
-	w-20
-	-m-10
-	aspect-square
-	rounded-full
-	-z-10
-	bg-white
-	blur-2`xl
-	"
-	id="circle"
-	style="left: {x}px; top: {y}px;"
-></div>
-	<!-- bg-radial from-white from-0% to-gray-900 to-60% -->
+<!-- bg-radial from-white from-0% to-gray-900 to-60% -->
 
 <div class=" min-h-screen w-full grid grid-rows-[auto,1fr,auto]">
 	<div class="">top</div>
@@ -37,6 +25,38 @@
 		middle
 		<br />
 		<div class="relative">
+			<div
+				bind:this={circleClip}
+				class="
+				absolute
+				left-0
+				w-full
+				bg-pink-500
+				rounded-full
+				px-5
+				z-100
+				text-4xl
+				pointer-events-none
+				overflow-clip
+			"
+			>
+				<p class="opacity-0">.</p>
+				<div
+					class="
+						absolute
+						w-20
+						-m-10
+						aspect-square
+						rounded-full
+						-z-10
+						bg-white
+						blur-xl
+						"
+					id="circle"
+					style="left: {x}px; top: {y}px;"
+				></div>
+			</div>
+
 			<div
 				class="
 				absolute
